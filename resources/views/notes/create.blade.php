dd12371401d0c637d7741ddd83ab15034b137606
ken.calvins.o <kennedyc@ringier.co.za>
2024-04-03 17:43:00 +0300

    More MR adjustments on the notes  create blade file

--- resources/views/notes/create.blade.php
+++ resources/views/notes/create.blade.php
@@ -5,10 +5,11 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-2xl mx-auto space-y-4 sm:px-6 lg:px-8">
-            <x-button icon="arrow-left" class="mb-8" href="{{ route('notes.index') }}">All Notes</x-button>
+    <div class="py-6 sm:py-12"> <!-- Reduced padding for smaller screens -->
+        <div class="max-w-xl mx-auto px-4 sm:px-6 lg:px-8 space-y-4"> <!-- Adjusted max-width and padding -->
+            <x-button icon="arrow-left" class="mb-4 sm:mb-8" href="{{ route('notes.index') }}">All Notes</x-button> <!-- Adjusted margin-bottom -->
             <livewire:notes.create-note />
         </div>
     </div>
+
 </x-app-layout>
